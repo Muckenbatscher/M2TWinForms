fc5fa9ab0b0b4e1e18bc364d6db2f139874d436f
Muckenbatscher <christi.braeuner@gmx.de>
2025-10-25 23:53:32 +0200

    improve assembly scan performance by checking the entry assembly first also return as soon as the first implementation was found

--- M2TWinForms.Themes/ThemeLoading/DefaultThemeLoadingManager.cs
+++ M2TWinForms.Themes/ThemeLoading/DefaultThemeLoadingManager.cs
@@ -12,31 +12,69 @@ namespace M2TWinForms.Themes.ThemeLoading
     {
         public static IThemeProvider? FindDefaultThemeProvider()
         {
-            var themeProviders = new List<IThemeProvider>();
-            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            var assemblies = GetOrderedRelevantAssemblies();
             foreach (var assembly in assemblies)
             {
                 var assemblyThemeProviders = FindDefaultThemeProvidersInAssembly(assembly);
-                themeProviders.AddRange(assemblyThemeProviders);
+                if (assemblyThemeProviders.Any())
+                    return assemblyThemeProviders.First();
             }
+            return null;
+        }
 
-            return themeProviders.FirstOrDefault();
+        private static IEnumerable<Assembly> GetOrderedRelevantAssemblies()
+        {
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            var relevantAssemblies = assemblies.Where(IsRelevantAssembly);
+
+            var entry = Assembly.GetEntryAssembly();
+            Assembly? main = null;
+            if (entry != null && IsRelevantAssembly(entry))
+                main = entry;
+
+            var ordered = relevantAssemblies
+                .OrderBy(a => a.GetName().Name)
+                .ToList();
+
+            if (main == null)
+                return ordered;
+
+            IEnumerable<Assembly> orderedWithoutMain;
+            var mainName = main.GetName().Name;
+            if (!string.IsNullOrEmpty(mainName))
+                orderedWithoutMain = ordered
+                    .Where(a => !mainName.Equals(a.GetName().Name));
+            else
+                orderedWithoutMain = ordered;
+
+            return orderedWithoutMain.Prepend(main);
+        }
+        static bool IsRelevantAssembly(Assembly assembly)
+        {
+            var name = assembly.GetName().Name;
+            if (string.IsNullOrEmpty(name))
+                return false;
+
+            bool isSystem = name.StartsWith("System.", StringComparison.OrdinalIgnoreCase);
+            bool isMicrosoft = name.StartsWith("Microsoft.", StringComparison.OrdinalIgnoreCase);
+            return !isSystem && !isMicrosoft;
         }
 
         private static IEnumerable<IThemeProvider> FindDefaultThemeProvidersInAssembly(Assembly assembly)
         {
-            var types = assembly.GetTypes();
+            var types = assembly.GetTypes().OrderBy(t => t.Name);
             foreach (var type in types)
             {
-                if (typeof(IDefaultThemeProvider).IsAssignableFrom(type) && !type.IsInterface)
-                {
-                    var instance = (IThemeProvider?)Activator.CreateInstance(type);
-                    if (instance != null)
-                    {
-                        yield return instance;
-                    }
-                }
+                if (!IsDefaultThemeProvider(type))
+                    continue;
+
+                var instance = (IThemeProvider?)Activator.CreateInstance(type);
+                if (instance != null)
+                    yield return instance;
             }
         }
+        private static bool IsDefaultThemeProvider(Type type)
+            => typeof(IDefaultThemeProvider).IsAssignableFrom(type) &&
+                !type.IsInterface && !type.IsAbstract;
     }
 }
