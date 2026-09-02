7a7210f5c96623386959df9e8797aacd38fbd981
Muckenbatscher <christi.braeuner97@gmail.com>
2026-09-02 11:22:38 +0200

    Use MaterialTheming.Winforms and use its conversion
    Don't implement the conversion from `MaterialTheming.RgbColor` to `System.Drawing.Color` again.

--- M2TWinForms/Themes/ThemeProviders/DefaultLightThemeProvider.cs
+++ M2TWinForms/Themes/ThemeProviders/DefaultLightThemeProvider.cs
@@ -6,18 +6,13 @@ namespace M2TWinForms.Themes.ThemeProviders
     {
         public Theme CreateTheme()
         {
-            var color = GetRgbColor(Color.Khaki);
+            var color = Color.Khaki.ToRgbColor();
             var colors = ThemeBuilder.CreateFromSourceColor(color)
                 .WithMode(ThemeMode.Light)
                 .WithContrastLevel(ContrastLevel.Normal)
                 .WithVariant(Variant.TonalSpot)
                 .Build();
             return new Theme(isDark: false, colors: colors);
-        }
-
-        private static RgbColor GetRgbColor(Color color)
-        {
-            return RgbColor.FromRgb(color.R, color.G, color.B);
         }
     }
 }
